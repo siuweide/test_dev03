049a0988c09345b311057a57d67ce0e0a768d7bb
siuweide <328527390@qq.com>
2019-11-16 22:28:42 +0800

    add files

---
+++ test_platform/app_personal/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            # return render(request, 'manage.html')
+            auth.login(request, user) #记录用户的登录状态
+            return redirect('/manage/')
+        elif username == '' or password == '':
+            error = 'username or password is not null'
+            return render(request, 'login.html', {
+                'error':error
+            })
+        else:
+            error = 'username or password is error!'
+            return render(request,'login.html',{
+                'error':error
+            })
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/login/')
+
+
+def manage(request):
+    return render(request, 'manage.html')
